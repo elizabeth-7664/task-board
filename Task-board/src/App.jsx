8997117810711b04222254b1aa6c8c117f46a9f2
Mammet-tech <edwinmammet19@gmail.com>
2025-04-23 17:30:42 +0300

    Add h2

--- Task-board/src/App.jsx
+++ Task-board/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-4xl font-bold text-white bg-black' >can you see this</h1>
+      <h1 className="text-4xl font-bold text-white bg-black">can you see</h1>
+      <h2>hello world</h2>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
