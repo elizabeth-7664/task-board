8a9fa5eae3f8ca59799149be736c1c7e7024afee
wilfredkiprop <kipropwilfred90@gmail.com>
2025-04-23 17:20:26 +0300

    exp

--- Task-board/src/App.jsx
+++ Task-board/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='text-4xl font-bold text-white bg-black' >hello world </h1>
+      <h1 className='text-4xl font-bold text-white bg-black' >hello world this is  a simple task-board</h1>
     </div>
   )
 }
